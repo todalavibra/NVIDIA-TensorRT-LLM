1f6f9836eff431df9bea3e1c6eebcd3a0273b80d
ApsarasX <apsarax@outlook.com>
2023-12-25 15:45:55 +0800

    refactor: use torch to get device capability considering older NVIDIA driver

--- benchmarks/python/base_benchmark.py
+++ benchmarks/python/base_benchmark.py
@@ -26,10 +26,8 @@ from tensorrt_llm.quantization import QuantMode
 
 
 def get_compute_cap():
-    output = subprocess.check_output(
-        ['nvidia-smi', "--query-gpu=compute_cap", "--format=csv"])
-    _, csv_value, *_ = output.splitlines()
-    return str(int(float(csv_value) * 10))
+    cap = torch.cuda.get_device_capability()
+    return str(cap[0] * 10 + cap[1])
 
 
 def get_csv_filename(model, dtype, tp_size, mode, **kwargs):
