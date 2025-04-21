d4118addee50194e6e0a80ef726e39c99da3688a
Daniel Afrimi <dafrimi@nvidia.com>
2025-04-21 12:47:31 +0000

    add latency support for trtllm bench
    Signed-off-by: Daniel Afrimi <dafrimi@nvidia.com>
    
    refactor
    
    Signed-off-by: Daniel Afrimi <dafrimi@nvidia.com>
    
    fix review
    
    Signed-off-by: Ubuntu <dafrimi@nvidia.com>
    
    refactor review
    
    Signed-off-by: Ubuntu <dafrimi@nvidia.com>

--- tensorrt_llm/bench/benchmark/throughput.py
+++ tensorrt_llm/bench/benchmark/throughput.py
@@ -18,7 +18,8 @@ from tensorrt_llm.bench.benchmark.utils.general import (
     get_settings_from_engine, get_settings)
 # isort: on
 from tensorrt_llm._torch.llm import LLM as PyTorchLLM
-from tensorrt_llm.bench.benchmark.utils.general import generate_warmup_dataset
+from tensorrt_llm.bench.benchmark.utils.general import (ALL_SUPPORTED_BACKENDS,
+                                                        generate_warmup_dataset)
 from tensorrt_llm.bench.dataclasses.configuration import RuntimeConfig
 from tensorrt_llm.bench.dataclasses.general import BenchmarkEnvironment
 from tensorrt_llm.bench.dataclasses.reporting import ReportUtility
@@ -43,7 +44,7 @@ from tensorrt_llm.sampling_params import SamplingParams
     help="Path to a serialized TRT-LLM engine.",
 )
 @optgroup.option("--backend",
-                 type=click.Choice(["pytorch", "_autodeploy"]),
+                 type=click.Choice(ALL_SUPPORTED_BACKENDS),
                  default=None,
                  help="Set to 'pytorch' for pytorch path. Default is cpp path.")
 @optgroup.option(
@@ -292,12 +293,8 @@ def throughput_command(
         logger.info(metadata.get_summary_for_print())
 
     # Engine configuration parsing
-    if backend and backend.lower() in ["pytorch", "_autodeploy"]:
-        # If we're dealing with a model name, perform a snapshot download to
-        # make sure we have a local copy of the model.
-        if checkpoint_path is None:
-            snapshot_download(model)
-
+    if backend and backend.lower() in ALL_SUPPORTED_BACKENDS and backend.lower(
+    ) != "cpp":
         exec_settings = get_settings(params, metadata, bench_env.model,
                                      bench_env.checkpoint_path)
         kwargs_max_sql = max_seq_len or metadata.max_sequence_length
